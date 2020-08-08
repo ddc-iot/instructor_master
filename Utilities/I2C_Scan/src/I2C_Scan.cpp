3aa01e882a4296b7fc0fffc5963be777cd749b0c
IOT Instructor <brashap@cnm.edu>
2020-08-08 16:47:57 -0600

    i2c_scan2

--- Utilities/I2C_Scan/src/I2C_Scan.cpp
+++ Utilities/I2C_Scan/src/I2C_Scan.cpp
@@ -2,7 +2,7 @@
 //       THIS IS A GENERATED FILE - DO NOT EDIT       //
 /******************************************************/
 
-#line 1 "/home/brian/Particle/I2C_Scan/src/I2C_Scan.ino"
+#line 1 "c:/Users/IoTPa/Documents/IoT/cohort2/instructor_master/Utilities/I2C_Scan/src/I2C_Scan.ino"
 /*
  * Project I2C_Scan
  * Description: Scan I2C bus and return found addresses
@@ -14,7 +14,10 @@
 
 void setup();
 void loop();
-#line 10 "/home/brian/Particle/I2C_Scan/src/I2C_Scan.ino"
+#line 10 "c:/Users/IoTPa/Documents/IoT/cohort2/instructor_master/Utilities/I2C_Scan/src/I2C_Scan.ino"
+byte error; 
+byte address;
+int nDevices;
 unsigned long delayTime;
 
 void setup()
@@ -23,54 +26,27 @@ void setup()
 	Serial.begin(9600);
 	while(!Serial);    // time to get serial running
 	Serial.println("\nI2C Scanner");
-
-	    unsigned status;
-    
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
 }
 
 
 void loop()
 {
-	byte error, address;
-	int nDevices;
-
-	Serial.println("Scanning...");
-	delay(1000);
-
-
+	Serial.printf("Scanning: \n--------- \n");
 	nDevices = 0;
-	for(address = 1; address < 127; address++ )
-	{
-		// The i2c_scanner uses the return value of
-		// the Write.endTransmisstion to see if
-		// a device did acknowledge to the address.
+	for(address = 1; address < 127; address++ ) {
 		Wire.beginTransmission(address);
 		error = Wire.endTransmission();
 
-		if (error == 0)
-		{
-			Serial.print("I2C device found at address 0x");
-			if (address<16)
-				Serial.print("0");
-			Serial.print(address,HEX);
-			Serial.println("  !");
-
+		if (error == 0) {
+			Serial.printf("I2C device found at address 0x%2x \n",address);
 			nDevices++;
-		}
-		else if (error==4)
-		{
-			Serial.print("Unknow error at address 0x");
-			if (address<16)
-				Serial.print("0");
-			Serial.println(address,HEX);
+		} else if (error==4) {
+			Serial.printf("Unknown error at address 0x%2x \n",address);
 		}
 	}
-	if (nDevices == 0)
-		Serial.println("No I2C devices found\n");
-	else
-		Serial.println("done\n");
-
-	delay(5000);           // wait 5 seconds for next scan
+	if (nDevices == 0) {
+		Serial.printf("No I2C devices found\n");
+	}
+	Serial.printf("Scan Complete!\n");
+	delay(delayTime);           // wait 5 seconds for next scan
 }
--- Utilities/I2C_Scan/src/I2C_Scan.ino
+++ Utilities/I2C_Scan/src/I2C_Scan.ino
@@ -7,62 +7,35 @@
 
 #include "Particle.h"
 
+byte error; 
+byte address;
+int nDevices;
 unsigned long delayTime;
 
-void setup()
-{
+void setup() {
 	Wire.begin();
 	Serial.begin(9600);
 	while(!Serial);    // time to get serial running
 	Serial.println("\nI2C Scanner");
-
-	    unsigned status;
-    
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
 }
 
 
-void loop()
-{
-	byte error, address;
-	int nDevices;
-
-	Serial.println("Scanning...");
-	delay(1000);
-
-
+void loop() {
+	Serial.printf("Scanning: \n--------- \n");
 	nDevices = 0;
-	for(address = 1; address < 127; address++ )
-	{
-		// The i2c_scanner uses the return value of
-		// the Write.endTransmisstion to see if
-		// a device did acknowledge to the address.
+	for(address = 1; address < 127; address++ ) {
 		Wire.beginTransmission(address);
 		error = Wire.endTransmission();
-
-		if (error == 0)
-		{
-			Serial.print("I2C device found at address 0x");
-			if (address<16)
-				Serial.print("0");
-			Serial.print(address,HEX);
-			Serial.println("  !");
-
+		if (error == 0) {
+			Serial.printf("I2C device found at address 0x%2x \n",address);
 			nDevices++;
-		}
-		else if (error==4)
-		{
-			Serial.print("Unknow error at address 0x");
-			if (address<16)
-				Serial.print("0");
-			Serial.println(address,HEX);
+		} else if (error == 4) {
+			Serial.printf("Unknown error at address 0x%2x \n",address);
 		}
 	}
-	if (nDevices == 0)
-		Serial.println("No I2C devices found\n");
-	else
-		Serial.println("done\n");
-
-	delay(5000);           // wait 5 seconds for next scan
+	if (nDevices == 0) {
+		Serial.printf("No I2C devices found\n");
+	}
+	Serial.printf("Scan Complete!\n");
+	delay(delayTime);           // wait 5 seconds for next scan
 }
